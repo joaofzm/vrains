cdf9bf0873c572ae6662059f18c4edbdab0c4986
joaofzm <joao.fzm@hotmail.com>
2021-03-12 02:07:04 -0300

    Update Main.java

--- src/menu/Main.java
+++ src/menu/Main.java
@@ -1,48 +1,38 @@
 package menu;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import board.DuelFrame;
 import cards.CardList;
-import board.BoardControl;
-import board.HandControl;
-import cards.SmallCards;
 
-public class Main  {
+public class Main {
 
 	public static int damage;
-	
+	// main
 	public static int playerLifePoints = 8000;
 	public static int opponentLifePoints = 8000;
 	public static int round = 1;
-	
-	
-	public static void main(String[] args) {		
-		
+
+	public static void main(String[] args) {
+
 		CardList.cardList();
-		
+
 		new SelectYourOpponentFrame();
-		//new StartFrame();
-		
+		// new StartFrame();
+
 	}
 
-
 	public static void damagePlayer(int damage) {
-		playerLifePoints -= damage;		
-		DuelFrame.playerLP.setText("             "+Main.playerLifePoints);
-		}
-	
+		playerLifePoints -= damage;
+		DuelFrame.playerLP.setText("             " + Main.playerLifePoints);
+	}
+
 	public static void damageOpponent(int damage) {
-		opponentLifePoints -= damage;		
-		DuelFrame.opponentLP.setText("             "+Main.opponentLifePoints);
-		}
-	
+		opponentLifePoints -= damage;
+		DuelFrame.opponentLP.setText("             " + Main.opponentLifePoints);
+	}
+
 	public static void passRound() {
-		round++;		
-		DuelFrame.round.setText("     "+Main.round);
-		}
-	
-	
-	
-	
+		round++;
+		DuelFrame.round.setText("     " + Main.round);
+	}
+
 }
