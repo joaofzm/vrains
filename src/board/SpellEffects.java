699abc417ea377ccb835ef71a4dc0e8f591b040e
joaofzm <joao.fzm@hotmail.com>
2021-03-13 21:45:23 -0300

    Update SpellEffects.java

--- src/board/SpellEffects.java
+++ src/board/SpellEffects.java
@@ -2,10 +2,20 @@ package board;
 
 import javax.swing.Icon;
 
+import cards.SmallCards;
+import effects.DarkHole;
+import effects.MirrorForce;
+
 public class SpellEffects {
 
 	public static void spellEffects(Icon icon) {
+		if (icon == SmallCards.minidarkhole) {
+			DarkHole.darkHole();
+		}
 
+		if (icon == SmallCards.minimirrorforce) {
+			MirrorForce.mirrorForce();
+		}
 	}
 
 }
