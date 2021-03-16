cabe0c51da1cbc7afaff71916ba3e4e8ef079aef
joaofzm <joao.fzm@hotmail.com>
2021-03-15 21:44:23 -0300

    Update PotOfGreed.java

--- src/effects/PotOfGreed.java
+++ src/effects/PotOfGreed.java
@@ -1,5 +1,13 @@
 package effects;
 
+import cards.DrawCardPlayer;
+
 public class PotOfGreed {
+
+	public static boolean potOfGreed() {
+		DrawCardPlayer.drawCardPlayer();
+		DrawCardPlayer.drawCardPlayer();
+		return true;
+	}
 
 }
