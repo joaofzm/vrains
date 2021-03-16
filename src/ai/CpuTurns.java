c55b34d8317893ea09b4a1d06da55dab2550dcce
joaofzm <joao.fzm@hotmail.com>
2021-03-15 21:42:21 -0300

    Create CpuTurns.java

---
+++ src/ai/CpuTurns.java
@@ -0,0 +1,60 @@
+package ai;
+
+import board.DuelFrame;
+
+public class CpuTurns {
+
+	public static void disablePlayer() {
+		DuelFrame.playerHand1.setEnabled(false);
+		DuelFrame.playerHand2.setEnabled(false);
+		DuelFrame.playerHand3.setEnabled(false);
+		DuelFrame.playerHand4.setEnabled(false);
+		DuelFrame.playerHand5.setEnabled(false);
+		DuelFrame.playerHand6.setEnabled(false);
+		DuelFrame.playerHand7.setEnabled(false);
+		DuelFrame.playerHand8.setEnabled(false);
+		DuelFrame.playerHand9.setEnabled(false);
+		DuelFrame.playerHand10.setEnabled(false);
+		DuelFrame.playerHand11.setEnabled(false);
+		DuelFrame.playerHand12.setEnabled(false);
+		DuelFrame.playerMonsterZone1.setEnabled(false);
+		DuelFrame.playerMonsterZone2.setEnabled(false);
+		DuelFrame.playerMonsterZone3.setEnabled(false);
+		DuelFrame.playerMonsterZone4.setEnabled(false);
+		DuelFrame.playerMonsterZone5.setEnabled(false);
+		DuelFrame.playerSpellZone1.setEnabled(false);
+		DuelFrame.playerSpellZone2.setEnabled(false);
+		DuelFrame.playerSpellZone3.setEnabled(false);
+		DuelFrame.playerSpellZone4.setEnabled(false);
+		DuelFrame.playerSpellZone5.setEnabled(false);
+		DuelFrame.nextPhaseButton.setEnabled(false);
+	}
+
+	public static void enablePlayer() {
+		DuelFrame.playerHand1.setEnabled(true);
+		DuelFrame.playerHand2.setEnabled(true);
+		DuelFrame.playerHand3.setEnabled(true);
+		DuelFrame.playerHand4.setEnabled(true);
+		DuelFrame.playerHand5.setEnabled(true);
+		DuelFrame.playerHand6.setEnabled(true);
+		DuelFrame.playerHand7.setEnabled(true);
+		DuelFrame.playerHand8.setEnabled(true);
+		DuelFrame.playerHand9.setEnabled(true);
+		DuelFrame.playerHand10.setEnabled(true);
+		DuelFrame.playerHand11.setEnabled(true);
+		DuelFrame.playerHand12.setEnabled(true);
+		DuelFrame.playerMonsterZone1.setEnabled(true);
+		DuelFrame.playerMonsterZone2.setEnabled(true);
+		DuelFrame.playerMonsterZone3.setEnabled(true);
+		DuelFrame.playerMonsterZone4.setEnabled(true);
+		DuelFrame.playerMonsterZone5.setEnabled(true);
+		DuelFrame.playerSpellZone1.setEnabled(true);
+		DuelFrame.playerSpellZone2.setEnabled(true);
+		DuelFrame.playerSpellZone3.setEnabled(true);
+		DuelFrame.playerSpellZone4.setEnabled(true);
+		DuelFrame.playerSpellZone5.setEnabled(true);
+		DuelFrame.nextPhaseButton.setEnabled(true);
+	}
+
+
+}
