aa82fd31b192886a0d9c0adca4ccbb1513197b49
joaofzm <joao.fzm@hotmail.com>
2021-03-15 21:44:16 -0300

    Update MonsterReborn.java

--- src/effects/MonsterReborn.java
+++ src/effects/MonsterReborn.java
@@ -1,5 +1,19 @@
 package effects;
 
+import board.BoardControl;
+import board.DuelFrame;
+import cards.SmallCards;
+
 public class MonsterReborn {
+
+	public static boolean monsterReborn() {
+
+		if (BoardControl.opponentMonster3Occupied == true) {
+			DuelFrame.opponentMonsterZone3.setIcon(null);
+			DuelFrame.opponentGraveyard.setIcon(SmallCards.minimaneaterbug);
+			BoardControl.opponentMonster3Occupied = false;
+		}
+		return true;
+	}
 
 }
