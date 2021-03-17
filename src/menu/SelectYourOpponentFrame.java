1e7c534232460f463f4f4fba661c2e288a39a7f0
joaofzm <joao.fzm@hotmail.com>
2021-03-17 04:41:39 -0300

    Update SelectYourOpponentFrame.java

--- src/menu/SelectYourOpponentFrame.java
+++ src/menu/SelectYourOpponentFrame.java
@@ -1,6 +1,7 @@
 package menu;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -81,6 +82,7 @@ public class SelectYourOpponentFrame extends JFrame implements ActionListener {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLayout(null);
 		this.setSize(1536,864);
+		this.setMinimumSize(new Dimension(1536, 864));
 		this.setVisible(true);
 		this.pack();
 
