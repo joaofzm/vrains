124ac7b3a3fe5274f0a069d850fb119147b1de69
joaofzm <joao.fzm@hotmail.com>
2021-03-18 03:31:53 -0300

    Update SmallCards.java

--- src/cards/SmallCards.java
+++ src/cards/SmallCards.java
@@ -4,33 +4,35 @@ import javax.swing.ImageIcon;
 
 public class SmallCards {
 	
-	public static ImageIcon minidarkelf = new ImageIcon("minidarkelf.jpg");
-	public static ImageIcon minidarkhole = new ImageIcon("minidarkhole.jpg");
-	public static ImageIcon minigeminielf = new ImageIcon("minigeminielf.jpg");
-	public static ImageIcon minilajinn = new ImageIcon("minilajinn.jpg");
-	public static ImageIcon minilusterdragon = new ImageIcon("minilusterdragon.jpg");
-	public static ImageIcon minimaneaterbug = new ImageIcon("minimaneaterbug.jpg");
-	public static ImageIcon minimirrorforce = new ImageIcon("minimirrorforce.jpg");
-	public static ImageIcon minimonsterreborn = new ImageIcon("minimonsterreborn.jpg");
-	public static ImageIcon minipotofgreed = new ImageIcon("minipotofgreed.jpg");
-	public static ImageIcon miniraigeki = new ImageIcon("miniraigeki.jpg");
-	public static ImageIcon minisevencoloredfish = new ImageIcon("minisevencoloredfish.jpg");
-	public static ImageIcon minisumonnedskull = new ImageIcon("minisumonnedskull.jpg");
-	public static ImageIcon miniswordsofrevealinglight = new ImageIcon("miniswordsofrevealinglight.jpg");
-	public static ImageIcon minicover = new ImageIcon("minicover.jpg");
-	public static ImageIcon minidefensecover = new ImageIcon("minidefensecover.jpg");
+	public static ImageIcon minidarkelf = new ImageIcon(SmallCards.class.getResource("minidarkelf.jpg"));
+	
+	public static ImageIcon minidarkhole = new ImageIcon(SmallCards.class.getResource("minidarkhole.jpg"));
+	public static ImageIcon minigeminielf = new ImageIcon(SmallCards.class.getResource("minigeminielf.jpg"));
+	public static ImageIcon minilajinn = new ImageIcon(SmallCards.class.getResource("minilajinn.jpg"));
+	public static ImageIcon minilusterdragon = new ImageIcon(SmallCards.class.getResource("minilusterdragon.jpg"));
+	public static ImageIcon minimaneaterbug = new ImageIcon(SmallCards.class.getResource("minimaneaterbug.jpg"));
+	public static ImageIcon minimirrorforce = new ImageIcon(SmallCards.class.getResource("minimirrorforce.jpg"));
+	public static ImageIcon minimonsterreborn = new ImageIcon(SmallCards.class.getResource("minimonsterreborn.jpg"));
+	public static ImageIcon minipotofgreed = new ImageIcon(SmallCards.class.getResource("minipotofgreed.jpg"));
+	public static ImageIcon miniraigeki = new ImageIcon(SmallCards.class.getResource("miniraigeki.jpg"));
+	public static ImageIcon minisevencoloredfish = new ImageIcon(SmallCards.class.getResource("minisevencoloredfish.jpg"));
+	public static ImageIcon minisumonnedskull = new ImageIcon(SmallCards.class.getResource("minisumonnedskull.jpg"));
+	public static ImageIcon miniswordsofrevealinglight = new ImageIcon(SmallCards.class.getResource("miniswordsofrevealinglight.jpg"));
+	public static ImageIcon minicover = new ImageIcon(SmallCards.class.getResource("minicover.jpg"));
+	public static ImageIcon minidefensecover = new ImageIcon(SmallCards.class.getResource("minidefensecover.jpg"));
 
-	public static ImageIcon minid = new ImageIcon("minid.jpg");
-	public static ImageIcon minie = new ImageIcon("minie.jpg");
-	public static ImageIcon minia = new ImageIcon("minia.jpg");
-	public static ImageIcon minit = new ImageIcon("minit.jpg");
-	public static ImageIcon minih = new ImageIcon("minih.jpg");
+	public static ImageIcon minid = new ImageIcon(SmallCards.class.getResource("minid.jpg"));
+	public static ImageIcon minie = new ImageIcon(SmallCards.class.getResource("minie.jpg"));
+	public static ImageIcon minia = new ImageIcon(SmallCards.class.getResource("minia.jpg"));
+	public static ImageIcon minit = new ImageIcon(SmallCards.class.getResource("minit.jpg"));
+	public static ImageIcon minih = new ImageIcon(SmallCards.class.getResource("minih.jpg"));
+	
+	public static ImageIcon miniberfomet = new ImageIcon(SmallCards.class.getResource("miniberfomet.jpg"));
+	public static ImageIcon minilamia = new ImageIcon(SmallCards.class.getResource("minilamia.jpg"));
+	public static ImageIcon miniswirlslime = new ImageIcon(SmallCards.class.getResource("miniswirlslime.jpg"));
+	public static ImageIcon mininecroslime = new ImageIcon(SmallCards.class.getResource("mininecroslime.jpg"));
+                
+	public static ImageIcon minitranslucid = new ImageIcon(SmallCards.class.getResource("translucid.png"));
 
-	public static ImageIcon miniberfomet = new ImageIcon("miniberfomet.jpg");
-	public static ImageIcon minilamia = new ImageIcon("minilamia.jpg");
-	public static ImageIcon miniswirlslime = new ImageIcon("miniswirlslime.jpg");
-	public static ImageIcon mininecroslime = new ImageIcon("mininecroslime.jpg");
-
-	public static ImageIcon translucid = new ImageIcon("translucid.png");
 
 }
