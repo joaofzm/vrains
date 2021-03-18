449d6376ba21a8d59a1c24d0f8ef338cca631eb0
joaofzm <joao.fzm@hotmail.com>
2021-03-18 03:29:06 -0300

    Update BigCards.java

--- src/cards/BigCards.java
+++ src/cards/BigCards.java
@@ -4,30 +4,31 @@ import javax.swing.ImageIcon;
 
 public class BigCards {
 
-	public static ImageIcon darkelf = new ImageIcon("darkelf.jpg");
-	public static ImageIcon darkhole = new ImageIcon("darkhole.jpg");
-	public static ImageIcon geminielf = new ImageIcon("geminielf.jpg");
-	public static ImageIcon lajinn = new ImageIcon("lajinn.jpg");
-	public static ImageIcon lusterdragon = new ImageIcon("lusterdragon.jpg");
-	public static ImageIcon maneaterbug = new ImageIcon("maneaterbug.jpg");
-	public static ImageIcon mirrorforce = new ImageIcon("mirrorforce.jpg");
-	public static ImageIcon monsterreborn = new ImageIcon("monsterreborn.jpg");
-	public static ImageIcon potofgreed = new ImageIcon("potofgreed.jpg");
-	public static ImageIcon raigeki = new ImageIcon("raigeki.jpg");
-	public static ImageIcon sevencoloredfish = new ImageIcon("sevencoloredfish.jpg");
-	public static ImageIcon sumonnedskull = new ImageIcon("sumonnedskull.jpg");
-	public static ImageIcon swordsofrevealinglight = new ImageIcon("swordsofrevealinglight.jpg");
-	public static ImageIcon cover = new ImageIcon("cover.jpg");
-	
-	public static ImageIcon d = new ImageIcon("d.jpg");
-	public static ImageIcon e = new ImageIcon("e.jpg");
-	public static ImageIcon a = new ImageIcon("a.jpg");
-	public static ImageIcon t = new ImageIcon("t.jpg");
-	public static ImageIcon h = new ImageIcon("h.jpg");
+	public static ImageIcon darkelf = new ImageIcon(BigCards.class.getResource("darkelf.jpg"));
 
-	public static ImageIcon berfomet = new ImageIcon("berfomet.jpg");
-	public static ImageIcon lamia = new ImageIcon("lamia.jpg");
-	public static ImageIcon swirlslime = new ImageIcon("swirlslime.jpg");
-	public static ImageIcon necroslime = new ImageIcon("necroslime.jpg");
+	public static ImageIcon darkhole = new ImageIcon(BigCards.class.getResource("darkhole.jpg"));
+	public static ImageIcon geminielf = new ImageIcon(BigCards.class.getResource("geminielf.jpg"));
+	public static ImageIcon lajinn = new ImageIcon(BigCards.class.getResource("lajinn.jpg"));
+	public static ImageIcon lusterdragon = new ImageIcon(BigCards.class.getResource("lusterdragon.jpg"));
+	public static ImageIcon maneaterbug = new ImageIcon(BigCards.class.getResource("maneaterbug.jpg"));
+	public static ImageIcon mirrorforce = new ImageIcon(BigCards.class.getResource("mirrorforce.jpg"));
+	public static ImageIcon monsterreborn = new ImageIcon(BigCards.class.getResource("monsterreborn.jpg"));
+	public static ImageIcon potofgreed = new ImageIcon(BigCards.class.getResource("potofgreed.jpg"));
+	public static ImageIcon raigeki = new ImageIcon(BigCards.class.getResource("raigeki.jpg"));
+	public static ImageIcon sevencoloredfish = new ImageIcon(BigCards.class.getResource("sevencoloredfish.jpg"));
+	public static ImageIcon sumonnedskull = new ImageIcon(BigCards.class.getResource("sumonnedskull.jpg"));
+	public static ImageIcon swordsofrevealinglight = new ImageIcon(BigCards.class.getResource("swordsofrevealinglight.jpg"));
+	public static ImageIcon cover = new ImageIcon(BigCards.class.getResource("cover.jpg"));
+
+	public static ImageIcon d = new ImageIcon(BigCards.class.getResource("d.jpg"));
+	public static ImageIcon e = new ImageIcon(BigCards.class.getResource("e.jpg"));
+	public static ImageIcon a = new ImageIcon(BigCards.class.getResource("a.jpg"));
+	public static ImageIcon t = new ImageIcon(BigCards.class.getResource("t.jpg"));
+	public static ImageIcon h = new ImageIcon(BigCards.class.getResource("h.jpg"));
+
+	public static ImageIcon berfomet = new ImageIcon(BigCards.class.getResource("berfomet.jpg"));
+	public static ImageIcon lamia = new ImageIcon(BigCards.class.getResource("lamia.jpg"));
+	public static ImageIcon swirlslime = new ImageIcon(BigCards.class.getResource("swirlslime.jpg"));
+	public static ImageIcon necroslime = new ImageIcon(BigCards.class.getResource("necroslime.jpg"));
 
 }
